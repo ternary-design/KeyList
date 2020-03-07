7e40467176c2a0ebc5af6892e68749bf38d1dfcc
ternary-design <development@ternary-design.com>
2020-03-07 23:04:51 +0900

    Initial Commit

---
+++ KeyList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KeyList
+//
+//  Created by 鈴木健太 on 2020/03/07.
+//  Copyright © 2020 鈴木健太. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
